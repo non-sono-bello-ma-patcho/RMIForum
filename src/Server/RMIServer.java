cfd94cbce9bcca9184d9affb84dcd59ed71e1c47
rollingflamingo <andrea.storace.uni@gmail.com>
2018-05-20 16:45:32 +0200

    added poolClass, in case @PageFaultHandler doesn't know how a executor service works....

--- src/Server/RMIServer.java
+++ src/Server/RMIServer.java
@@ -9,36 +9,45 @@ import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.HashMap;
-import java.util.List;
 
 public class RMIServer implements core.RMIServer {
     private HashMap<String, TopicClass> Topics;
     private HashMap<String, RMIClient> ClientList;
+    private HashMap<String, String> Credential;
     final int clientPort = 1968;
 
-    private RMIClient getRemoteMethod(String host, int port) throws RemoteException, NotBoundException {
+    private RMIClient getRemoteMethod(String host) throws RemoteException, NotBoundException {
         System.err.println("Trying to retrieve registry from host...");
-        Registry registry = LocateRegistry.getRegistry(host, port);
+        Registry registry = LocateRegistry.getRegistry(host, clientPort);
         System.err.println("LookingUp for share Object");
         return (RMIClient) registry.lookup("RMISharedClient");
     }
 
     @Override
-    public boolean ManageConnection(String username, String password, String address, String op) {
+    public synchronized boolean ManageConnection(String username, String password, String address, String op) {
         // init convo with client...
-
-        return false;
+        try {
+            RMIClient stub = getRemoteMethod(address);
+            if(ClientList.containsKey(username)) return false;
+            ClientList.putIfAbsent(username, stub);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
+        Credential.put(username, password);
+        return true;
     }
 
     @Override
-    public boolean ManageSubscribe(String TopicLabel, String User, boolean unsubscribe) {
+    public synchronized boolean ManageSubscribe(String TopicLabel, String User, boolean unsubscribe) {
         if(!unsubscribe) return Topics.get(TopicLabel).addUser(User);
         else return Topics.get(TopicLabel).RemoveUser(User);
     }
 
     @Override
     public void Notify() {
-        // call remotely users methods....
+        // call remotely users methods for all client registered...0
     }
 
     @Override
