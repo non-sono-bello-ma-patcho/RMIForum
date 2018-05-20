0dd11d30f7f74982699e0e76fc7f15dbb6ae0710
rollingflamingo <andrea.storace.uni@gmail.com>
2018-05-20 13:57:47 +0200

    Server Interface must extends Remote

--- src/core/RMIServer.java
+++ src/core/RMIServer.java
@@ -1,6 +1,9 @@
 package core;
+
+import java.rmi.Remote;
+
 /* seniti libero di fare le modifiche che vuoi.. ho solo messo per ora dei parametri che mi venivano bene nel lato client*/
-public interface RMIServer {
+public interface RMIServer extends Remote {
     boolean ManageConnection(String username,String password, String op);
     boolean ManageSubscribe(String TopicLabel, boolean unsubscribe); /* metodo per iscrizione a topic... manca la dichiarazione di un metdodo per iscrizione al forum stesso*/
     void Notify(); // triggers message show on client...
