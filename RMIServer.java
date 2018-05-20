3722fe792e9f5766c8d8171ce1cc383aab378b37
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2018-05-20 13:19:41 +0200

    deleted that file.. wrong commit

--- RMIServer.java
+++
@@ -1,9 +0,0 @@
-package core;
-/* seniti libero di fare le modifiche che vuoi.. ho solo messo per ora dei parametri che mi venivano bene nel lato client*/
-public interface RMIServer {
-    boolean ManageConnection(String usurname,String password, String op);
-    boolean ManageSubscribe(String usurname, String password);
-    boolean ManageTopicSubscribe(String TopicLabel); /* metodo per iscrizione a topic..*/
-    void Notify();
-    void ManagePublish();
-}
