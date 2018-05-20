149a9205df1b184254bcdcd05dc47c7ea6dcb0be
rollingflamingo <andrea.storace.uni@gmail.com>
2018-05-20 17:04:28 +0200

    added server RMI setup, need to fix minor issues, maybe we're ready to compile...

--- src/Server/RMIServer.java
+++ src/Server/RMIServer.java
@@ -1,26 +1,89 @@
 package Server;
 
 import core.MessageClass;
+import core.PoolClass;
 import core.RMIClient;
 import core.TopicClass;
 
+import java.rmi.AlreadyBoundException;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class RMIServer implements core.RMIServer {
     private HashMap<String, TopicClass> Topics;
     private HashMap<String, RMIClient> ClientList;
     private HashMap<String, String> Credential;
+    private PoolClass pool;
     final int clientPort = 1968;
+
+    private void serverSetUp(){
+        System.setProperty("java.security.policy", "/tmp/RMIServer.policy");
+        if (System.getSecurityManager()==null) System.setSecurityManager(new SecurityManager());
+        RMIServer obj = new RMIServer();
+        Registry reg;
+        String alias = "RMISharedServer";
+        try {
+            reg=setRegistry(clientPort);
+            ExportNBind(reg, obj, alias,clientPort);
+
+            System.err.println("Server ready, type something to shutdown...");
+            Scanner sc = new Scanner(System.in);
+            System.err.println("You typed: "+sc.next());
+            shutDown(reg, obj, alias);
+        } catch (RemoteException e) {
+            System.err.println("Couldn't set registry, maybe you want to check stack trace?[S/n]");
+            // showStackTrace(e);
+        } catch (AlreadyBoundException e) {
+            System.err.println("Couldn't export and bind, maybe you want to check stack trace?[S/n]");
+            // showStackTrace(e);
+        } catch (NotBoundException e) {
+            System.err.println("Couldn't unexport and unbind, maybe you want to check stack trace?[S/n]");
+            // showStackTrace(e);
+        }
+    }
 
     private RMIClient getRemoteMethod(String host) throws RemoteException, NotBoundException {
         System.err.println("Trying to retrieve registry from host...");
         Registry registry = LocateRegistry.getRegistry(host, clientPort);
         System.err.println("LookingUp for share Object");
         return (RMIClient) registry.lookup("RMISharedClient");
+    }
+
+    private Registry setRegistry(int port) throws RemoteException {
+        try {
+            return LocateRegistry.createRegistry(port);
+        } catch (RemoteException e) {
+            return LocateRegistry.getRegistry(port);
+        }
+    }
+
+    private void ExportNBind(Registry reg, RMIServer obj, String alias, int port) throws AlreadyBoundException, RemoteException {
+        RMIServer stub = (RMIServer) UnicastRemoteObject.exportObject(obj, port);
+        reg.bind(alias, stub);
+    }
+
+    private void RMIshutDown(Registry reg,RMIServer obj, String alias) throws RemoteException, NotBoundException {
+        reg.unbind(alias);
+        UnicastRemoteObject.unexportObject(obj, true);
+    }
+
+    public RMIServer(){
+        Topics = new HashMap<>();
+        ClientList = new HashMap<>();
+        Credential = new HashMap<>();
+        pool = new PoolClass();
+
+        // here start the server...
+        serverSetUp();
+    }
+
+    public void shutDown(){
+        RMIshutDown();
     }
 
     @Override
