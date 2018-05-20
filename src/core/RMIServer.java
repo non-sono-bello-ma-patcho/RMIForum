5189f6fa7e84bfda087c7733d8dcae84662d2cd4
Savona Alessandro <36107882+PageFaultHandler@users.noreply.github.com>
2018-05-20 13:05:11 +0200

    client class updated
    the method subscribe request is not finished

--- src/core/RMIServer.java
+++ src/core/RMIServer.java
@@ -2,7 +2,8 @@ package core;
 /* seniti libero di fare le modifiche che vuoi.. ho solo messo per ora dei parametri che mi venivano bene nel lato client*/
 public interface RMIServer {
     boolean ManageConnection(String usurname,String password, String op);
-    boolean ManageSubscribe(String TopicLabel); /* metodo per iscrizione a topic... manca la dichiarazione di un metdodo per iscrizione al forum stesso*/
+    boolean ManageSubscribe();
+    boolean ManageTopicSubscribe(String TopicLabel); /* metodo per iscrizione a topic..*/
     void Notify();
     void ManagePublish();
 }
