e9e01c04ba821e8725164e517ef067929ff3736f
rollingflamingo <andrea.storace.uni@gmail.com>
2018-07-04 15:19:22 +0200

    removed unused import

--- src/Test/ServerTester.java
+++ src/Test/ServerTester.java
@@ -1,19 +1,12 @@
 package Test;
 
-import RMICore.MessageClass;
 import RMICore.TopicList;
 import Server.RMIServer;
 import user.User;
 
-import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.rmi.NotBoundException;
-import java.rmi.Remote;
 import java.rmi.RemoteException;
-import java.util.Scanner;
-import java.util.concurrent.ConcurrentHashMap;
-
-import static java.lang.Thread.sleep;
 
 public class ServerTester extends RMIServer {
     private User clientSide;
